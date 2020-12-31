e59c11d72619c09a9397069843f12948850f7883
Ari Greenberg <arigreenberg@gmail.com>
2020-12-31 08:49:58 -0800

    More sorbet typechecks added

--- koans/about_nil.rb
+++ koans/about_nil.rb
@@ -1,17 +1,19 @@
-# typed: false
+# typed: strict
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutNil < Neo::Koan
+  sig {void}
   def test_nil_is_an_object
     assert_equal true, nil.is_a?(Object), "Unlike NULL in other languages"
   end
 
+  sig {void}
   def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
     # What happens when you call a method that doesn't exist.  The
     # following begin/rescue/end code block captures the exception and
     # makes some assertions about it.
     begin
-      nil.some_method_nil_doesnt_know_about
+      T.unsafe(nil).some_method_nil_doesnt_know_about
     rescue Exception => ex
       # What exception has been caught?
       assert_equal NoMethodError, ex.class
@@ -22,6 +24,7 @@ class AboutNil < Neo::Koan
     end
   end
 
+  sig {void}
   def test_nil_has_a_few_methods_defined_on_it
     assert_equal true, nil.nil?
     assert_equal '', nil.to_s
