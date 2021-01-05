56a8fcfee4c9cc896e61303bc3be540fa47a9291
Ari Greenberg <arigreenberg@gmail.com>
2021-01-04 17:10:22 -0800

    Solved and typechecked about_modules

--- koans/about_modules.rb
+++ koans/about_modules.rb
@@ -1,48 +1,62 @@
-# typed: false
+# typed: strict
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutModules < Neo::Koan
+
   module Nameable
+    extend T::Sig
+
+    sig {params(new_name: String).void}
     def set_name(new_name)
-      @name = new_name
+      # TODO: Can modules access instance variables?
+      @name = T.let(new_name, T.nilable(String))
     end
 
+    sig {returns(Symbol)}
     def here
       :in_module
     end
   end
 
+  sig {void}
   def test_cant_instantiate_modules
-    assert_raise(___) do
-      Nameable.new
+    assert_raise(NoMethodError) do
+      T.unsafe(Nameable).new
     end
   end
 
   # ------------------------------------------------------------------
 
   class Dog
+    extend T::Sig
     include Nameable
 
+    sig {returns(String)}
     attr_reader :name
 
+    sig {void}
     def initialize
-      @name = "Fido"
+      @name = T.let("Fido", String)
     end
 
+    sig {returns(String)}
     def bark
       "WOOF"
     end
 
+    sig {returns(Symbol)}
     def here
       :in_object
     end
   end
 
+  sig {void}
   def test_normal_methods_are_available_in_the_object
     fido = Dog.new
-    assert_equal __, fido.bark
+    assert_equal "WOOF", fido.bark
   end
 
+  sig {void}
   def test_module_methods_are_also_available_in_the_object
     fido = Dog.new
     assert_nothing_raised do
@@ -50,15 +64,17 @@ class AboutModules < Neo::Koan
     end
   end
 
+  sig {void}
   def test_module_methods_can_affect_instance_variables_in_the_object
     fido = Dog.new
-    assert_equal __, fido.name
+    assert_equal "Fido", fido.name
     fido.set_name("Rover")
-    assert_equal __, fido.name
+    assert_equal "Rover", fido.name
   end
 
+  sig {void}
   def test_classes_can_override_module_methods
     fido = Dog.new
-    assert_equal __, fido.here
+    assert_equal :in_object, fido.here
   end
 end
